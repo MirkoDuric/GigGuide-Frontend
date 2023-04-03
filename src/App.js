821cc4ffec9fde0b5003d9f426d54805fe0c1192
ebro1230 <ebro1230@gmail.com>
2023-04-03 20:29:56 +0200

    again saving while checking out another branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router";
+import DisplayCarousel from "./Components/DisplayCarousel";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DisplayCarousel />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
