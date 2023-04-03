53ad48cd200f0e7b88370b0e898f0393d6ae946f
ebro1230 <ebro1230@gmail.com>
2023-04-03 22:49:42 +0200

    works with ticketmaster query objects too

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -4,6 +4,8 @@ import Figure from "react-bootstrap/Figure";
 
 const Event = (props) => {
   //const bands = props.bands;
+  //const type = props.type;
+  const type = "";
   const bands = [
     {
       _id: "6425f346b08f3d401bad0c77",
@@ -94,10 +96,93 @@ const Event = (props) => {
     },
   ];
 
+  const shania = [
+    {
+      name: "Shania Twain: Queen Of Me Tour",
+      type: "event",
+      id: "G5eVZ94L3b7SJ",
+      test: false,
+      url: "https://concerts.livenation.com/shania-twain-queen-of-me-tour-charlotte-north-carolina-06-28-2023/event/2D005D57D2604CE6",
+      locale: "en-us",
+      images: [
+        {
+          ratio: "3_2",
+          url: "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-4dec-b1d6-539e44a521d1_1825531_RETINA_PORTRAIT_3_2.jpg",
+          width: 640,
+          height: 427,
+          fallback: false,
+        },
+        {
+          ratio: "16_9",
+          url: "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-4dec-b1d6-539e44a521d1_1825531_EVENT_DETAIL_PAGE_16_9.jpg",
+          width: 205,
+          height: 115,
+          fallback: false,
+        },
+      ],
+      sales: {},
+      dates: {
+        start: {
+          localDate: "2023-06-28",
+          localTime: "19:30:00",
+          dateTime: "2023-06-28T23:30:00Z",
+          dateTBD: false,
+          dateTBA: false,
+          timeTBA: false,
+          noSpecificTime: false,
+        },
+      },
+      classifications: [],
+      promoter: {},
+      promoters: [],
+      info: "Show 7:30pm. This is an outdoor venue with portions of the reserved seats under cover. The show will take place rain or shine. All dates, acts, & ticket prices subject to change without notice. All tickets are subject to applicable service fees via all points of sale. Children under 2 are free as long as they don't take up a seat.",
+      pleaseNote:
+        "Show 7:30pm. This is an outdoor venue with portions of the reserved seats under cover. The show will take place rain or shine. All dates, acts, & ticket prices subject to change without notice. All tickets are subject to applicable service fees via all points of sale. Children under 2 are free as long as they don't take up a seat.",
+      priceRanges: [],
+      products: [],
+      seatmap: {},
+      accessibility: {},
+      ticketLimit: {},
+      ageRestrictions: {},
+      ticketing: {},
+      _links: {},
+      _embedded: {
+        venues: [
+          {
+            name: "PNC Music Pavilion",
+            type: "venue",
+            id: "KovZpZAEkeJA",
+            test: false,
+            url: "https://www.ticketmaster.com/pnc-music-pavilion-tickets-charlotte/venue/368716",
+            locale: "en-us",
+            aliases: [],
+            images: [],
+            postalCode: "28262",
+            timezone: "America/New_York",
+            city: {
+              name: "Charlotte",
+            },
+            state: {
+              name: "North Carolina",
+              stateCode: "NC",
+            },
+            country: {
+              name: "United States Of America",
+              countryCode: "US",
+            },
+            address: {
+              line1: "707 Pavilion Boulevard",
+            },
+          },
+        ],
+      },
+    },
+  ];
+
   return (
     <Stack gap={2}>
       {bands.length
-        ? bands.type !== "local"
+        ? type !== "local"
           ? bands.map((band) => {
               return band.upcomingEvents ? (
                 band.upcomingEvents.length ? (
@@ -143,40 +228,38 @@ const Event = (props) => {
                 ) : null
               ) : null;
             })
-          : bands.map((band) => {
+          : shania.map((band) => {
               return (
                 <a href={band._embedded.venues[0].url}>
                   <div className="row">
-                    <div className="col">
+                    <div className="col-2">
                       <Figure>
                         <Figure.Image
-                          width={171}
-                          height={180}
+                          width={"100%"}
                           alt="171x180"
-                          src={band.images[1]}
+                          src={band.images[1].url}
                         />
                       </Figure>
                     </div>
                     <div className="col">
                       <Card>
+                        <Card.Header>
+                          <h2>{band.name}</h2>
+                        </Card.Header>
                         <Card.Body>
                           <div className="row">
-                            <h3>{band.name}</h3>
-                          </div>
-                          <div className="row">
-                            <h6>{band._embedded.venues[0].name}</h6>
-                            <br />
-                            <p>
-                              {band._embedded.venues[0].address},{" "}
-                              {band._embedded.venues[0].city},{" "}
-                              {band._embedded.venues[0].state}{" "}
-                              {band._embedded.venues[0].postalCode}
-                            </p>
-                          </div>
-                          <div className="row">
-                            <p className="eventDate">
+                            <h4>
+                              {band._embedded.venues[0].name} -{" "}
                               {band.dates.start.localDate},{" "}
                               {band.dates.start.localTime}
+                            </h4>
+
+                            <p className="venueAddress">
+                              {band._embedded.venues[0].address.line1},{" "}
+                              {band._embedded.venues[0].city.name},{" "}
+                              {band._embedded.venues[0].state.name}{" "}
+                              {band._embedded.venues[0].postalCode},{" "}
+                              {band._embedded.venues[0].country.name}
                             </p>
                           </div>
                           <div className="row">
