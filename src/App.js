0a3cd367f994683af0f031db61ccde84b6599eda
MirkoDuric <mirkodjuricczv@gmail.com>
2023-04-04 21:34:08 +0200

    Having issues with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Router, Route } from "react-router-dom";
+import Signup from "./view/Signup";
+import Login from "./view/Login";
+import Profilepage from "./view/Profilepage";
+// import "./App.css";
+const App = () => {
+  return (
+    <Router>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/user/fan" element={<Profilepage />} />
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/view/Profilepage.js
@@ -0,0 +1,11 @@
+import Image from "react-bootstrap/Image";
+
+const Profilepage = () => {
+  return (
+    <div className="profile-container">
+      <Image src alt="Banner Image" roundedCircle />
+    </div>
+  );
+};
+
+export default Profilepage;
