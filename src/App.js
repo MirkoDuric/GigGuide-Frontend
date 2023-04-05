c0a8d4007341bfcbaaa811288efaf46e0daaa6f4
MirkoDuric <mirkodjuricczv@gmail.com>
2023-04-05 20:18:14 +0200

    Conflict from App.js resolved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Signup from "./view/Signup";
 import Login from "./view/Login";
-<<<<<<< HEAD
 import FanProfilepage from "./view/FanProfilepage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LandingPage from "./Views/LandingPage";
@@ -15,17 +14,6 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/user/fan" element={<FanProfilepage />} />
     </Routes>
-=======
-import "./App.css";
-const App = () => {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
->>>>>>> f6604ad27c0caaa946a41b581e492b9ae710a8aa
   );
 };
 
