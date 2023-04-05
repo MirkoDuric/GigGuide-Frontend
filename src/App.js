77c374dc93051c1df3460ba46702b0045fb51483
ebro1230 <ebro1230@gmail.com>
2023-04-05 18:48:46 +0200

    stealing App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router";
+import LandingPage from "./Views/LandingPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
