3910de0fff5f6f4784da43ec6d114e1e75b1f9b2
ebro1230 <ebro1230@gmail.com>
2023-04-06 20:39:51 +0200

    search bar logic working

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,41 +1,101 @@
 import React, { useState, useEffect } from "react";
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+import { getCountryCode, getGenreId, countryNames, genreNames } from "../utils";
+
 import axios from "axios";
 
 const SearchBar = (props) => {
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState("");
+  const [genre, setGenre] = useState("");
+  const [city, setCity] = useState("");
+  const [countryCode, setCountryCode] = useState("");
 
   const onChange = (e) => {
     setSearch(e.target.value);
-    console.log(search);
   };
 
-  const onSubmit = (e) => {
+  const onChangeCity = (e) => {
+    setCity(e.target.value);
+  };
+
+  const onChangeCountry = (e) => {
+    setCountryCode(getCountryCode(e.target.value));
+  };
+
+  const onChangeGenre = (e) => {
+    setGenre(getGenreId(e.target.value));
+  };
+
+  const onClick = (e) => {
     e.preventDefault();
-    axios.get("");
+
+    axios
+      .get(
+        `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_TICKETMASTER_API}&keyword=${search}&locale=*&sort=relevance,desc&city=${city}&countryCode=${countryCode}&segmentName=Music&genreId=${genre}`
+      )
+      .then((response) => {
+        console.log(response);
+        //setBands(response.data._embedded.events);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        //setIsLoading(false);
+      });
   };
 
   return (
-    <form>
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col-9 searchBardiv">
-          <input
+    <Form>
+      <Form.Group className="mb-3" controlId="search">
+        <Form.Control type="search" placeholder="Search" onChange={onChange} />
+      </Form.Group>
+      <Row className="mb-3">
+        <Col className="col-1" />
+        <Col className="col-3">
+          <Form.Control
             type="search"
-            className="form-control"
-            id="searchBarLandingPage"
-            aria-describedby="searchBarLandingPage"
-            placeholder="Search"
-            onChange={onChange}
+            placeholder="City"
+            onChange={onChangeCity}
           />
-        </div>
-        <div className="col-1 searchButtondiv">
-          <button type="submit" className="btn btn-primary" onSubmit={onSubmit}>
-            Search
-          </button>
-        </div>
-        <div className="col-1"></div>
-      </div>
-    </form>
+        </Col>
+        <Col className="col-4">
+          <Form.Select onChange={onChangeCountry} placeholder="Country">
+            <option key="blankChoice" hidden value>
+              {" "}
+              --Country--{" "}
+            </option>
+            <option>None</option>
+            {countryNames.map((countryName) => {
+              return <option key={countryName}>{countryName}</option>;
+            })}
+          </Form.Select>
+        </Col>
+        <Col className="col-3">
+          <Form.Select onChange={onChangeGenre} placeholder="Genre">
+            <option key="blankChoice" hidden value>
+              {" "}
+              --Genre--{" "}
+            </option>
+            <option>None</option>
+            {genreNames.map((genreName) => {
+              return <option>{genreName}</option>;
+            })}
+          </Form.Select>
+        </Col>
+        <Col className="col-1" />
+      </Row>
+      <Col className="submitbuttondiv">
+        <Button variant="primary" /* type="submit" */ onClick={onClick}>
+          Search
+        </Button>
+      </Col>
+    </Form>
   );
 };
 
