2073c3c4d4d8d165ce893a67973e456221cd0a11
ebro1230 <ebro1230@gmail.com>
2023-04-06 15:56:45 +0200

    stashing search updates

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchBar = (props) => {
   const [search, setSearch] = useState([]);
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.get("");
+  };
 
   return (
     <form>
@@ -14,10 +25,11 @@ const SearchBar = (props) => {
             id="searchBarLandingPage"
             aria-describedby="searchBarLandingPage"
             placeholder="Search"
+            onChange={onChange}
           />
         </div>
         <div className="col-1 searchButtondiv">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onSubmit={onSubmit}>
             Search
           </button>
         </div>
