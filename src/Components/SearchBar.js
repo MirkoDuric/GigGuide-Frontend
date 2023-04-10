a1bfef63971366917567ed128e6d6d56bf8b5b7a
ebro1230 <ebro1230@gmail.com>
2023-04-10 14:59:25 +0200

    added some stuff

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -13,8 +13,10 @@ import axios from "axios";
 const SearchBar = (props) => {
   const [search, setSearch] = useState("");
   const [genre, setGenre] = useState("");
+  const [genreId, setGenreId] = useState("");
   const [city, setCity] = useState("");
   const [countryCode, setCountryCode] = useState("");
+  const [country, setCountry] = useState("");
   const [searchType, setSearchType] = useState("mainstream");
 
   const onChange = (e) => {
@@ -26,30 +28,49 @@ const SearchBar = (props) => {
   };
 
   const onChangeCountry = (e) => {
+    setCountry(e.target.value);
     setCountryCode(getCountryCode(e.target.value));
   };
 
   const onChangeGenre = (e) => {
-    setGenre(getGenreId(e.target.value));
+    setGenre(e.target.value);
+    setGenreId(getGenreId(e.target.value));
   };
 
   const onClick = (e) => {
     e.preventDefault();
-
-    axios
-      .get(
-        `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_TICKETMASTER_API}&keyword=${search}&locale=*&sort=relevance,desc&city=${city}&countryCode=${countryCode}&segmentName=Music&genreId=${genre}`
-      )
-      .then((response) => {
-        console.log(response);
-        //setBands(response.data._embedded.events);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        //setIsLoading(false);
-      });
+    if (searchType === "mainstream") {
+      axios
+        .get(
+          `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_TICKETMASTER_API}&keyword=${search}&locale=*&sort=relevance,desc&city=${city}&countryCode=${countryCode}&segmentName=Music&genreId=${genreId}`
+        )
+        .then((response) => {
+          console.log(response);
+          //setBands(response.data._embedded.events);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          //setIsLoading(false);
+        });
+    }
+    if (searchType === "local") {
+      axios
+        .get(
+          `https://localhost:8000/artists/name=${search}&sort=relevance,desc&city=${city}&country=${country}&genre=${genre}`
+        )
+        .then((response) => {
+          console.log(response);
+          //setBands(response.data._embedded.events);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          //setIsLoading(false);
+        });
+    }
   };
 
   return (
@@ -97,7 +118,7 @@ const SearchBar = (props) => {
         </Col>
       </Row>
       <Row className="searchtypediv">
-        <Col>
+        <Col className="mainstreamdiv">
           <Form.Check
             inline
             label="Mainstream Artists"
