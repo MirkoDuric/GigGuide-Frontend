ba637d9f9231cd1766faefdcb2a9dcfb88f137b7
MirkoDuric <mirkodjuricczv@gmail.com>
2023-04-17 19:25:58 +0200

    90% of FanProfilepage and ArtistProfilepage finished.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./view/Login";
 import FanProfilepage from "./view/FanProfilepage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LandingPage from "./view/LandingPage";
+import ArtistProfilepage from "./view/ArtistProfilepage";
 
 // import "./App.css";
 const App = () => {
@@ -12,8 +13,8 @@ const App = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/user/fan/:id" element={<FanProfilepage />} />
-      <Route path="/artist/:id" element={<FanProfilepage />} />
+      <Route path="/user/fan" element={<FanProfilepage />} />
+      <Route path="/artist" element={<ArtistProfilepage />} />
     </Routes>
   );
 };
